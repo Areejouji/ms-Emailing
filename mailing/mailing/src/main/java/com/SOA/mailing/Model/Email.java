013bc21c3a7cc4df9178fc6c403cb4485f769943
manar.ayed@etudiant-enit.utm.tn <manarayed000@gmail.com>
2023-12-24 21:15:48 +0100

    last commit

--- mailing/mailing/src/main/java/com/SOA/mailing/Model/Email.java
+++ mailing/mailing/src/main/java/com/SOA/mailing/Model/Email.java
@@ -3,22 +3,29 @@ package com.SOA.mailing.Model;
 import com.SOA.mailing.Enum.StatusEmail;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.Type;
+import org.hibernate.mapping.TypeDef;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
 @Data
 @Entity
 @Table(name="TB_EMAIL")
-
 
 public class Email implements Serializable {
     private static final long serialVersionID=1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "email_id")
     private Long emailId;
     private String emailTo;
     private String subject;
     private LocalDateTime sendDateEmail;
     private StatusEmail statusEmail;
-
+    @Column(name = "replacement_data", columnDefinition = "jsonb")
+    private String replacementData;
+    private String attachment;
 }
--- mailing/mailing/src/test/java/com/SOA/mailing/MsEmailingApplicationTests.java
+++ mailing/mailing/src/test/java/com/SOA/mailing/MsEmailingApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.mailing;
+package com.SOA.mailing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
